18beef32f885449e0f66edbc3fb929ee6e627c0f
Gyaneshwar Babbili <gyaneshwarbabbili@gmail.com>
2026-05-08 12:34:23 +0530

    written app,py

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
-def greet(name):
-    print("hello")
-    print(f"welcomr {name} to the application")
-    print("Thank you")
-s=input()
-greet(s)
+from helpers import *
+n1=int(input("n1:"))
+n2=int(input("n2:"))
+
+print("select the below num")
+print("select num 1 for addition")
+print("select num 2 for sub")
+print("select num 3 for mul")
+print("select num 4 for power/exp")
+print("select num 5 for divison")
+
+operator=int(input())
+if operator ==1:
+    print(add(n1,n2))
+elif operator ==2:
+    print(sub(n1,n2))
+elif operator ==3:
+    print(mull(n1,n2))
+elif operator ==4:
+    print(division(n1,n2))
+elif operator ==5:
+    print(power(n1,n2))
+else:
+    print("invalid input please enter the correct input")
---
+++ helpers.py
@@ -0,0 +1,17 @@
+def greet(name):
+    print("hello")
+    print(f"welcomr {name} to the application")
+    print("Thank you")
+s=input()
+greet(s)
+
+def mull(a,b):
+    return a*b
+def sub(a,b):
+    return a-b
+def add(a,b):
+    return a+b
+def division(a,b):
+    return a/b
+def power(a,b):
+    return a**b
