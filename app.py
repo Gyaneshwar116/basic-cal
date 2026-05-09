403ff0e2ee84b15fc6dcf0e2abb86e0200b68e6e
Gyaneshwar Babbili <gyaneshwarbabbili@gmail.com>
2026-05-09 13:20:58 +0530

    Final version

--- app.py
+++ app.py
@@ -1,26 +1,30 @@
 from helpers import *
-n1=int(input("n1:"))
-n2=int(input("n2:"))
+import streamlit as st
 
-print("select the below num")
-print("select num 1 for addition")
-print("select num 2 for sub")
-print("select num 3 for mul")
-print("select num 4 for power/exp")
-print("select num 5 for divison")
-print("select num 6 for floor div")
-operator=int(input())
-if operator ==1:
-    print(add(n1,n2))
-elif operator ==2:
-    print(sub(n1,n2))
-elif operator ==3:
-    print(mull(n1,n2))
-elif operator ==4:
-    print(division(n1,n2))
-elif operator ==5:
-    print(power(n1,n2))
-elif operator ==6:
-    print(floor(n1,n2))
-else:
-    print("invalid input please enter the correct input")
+st.title("cal of two num")
+
+n1 =int(st.number_input("n1:"))
+#n1 = int(input("n1:"))
+n2 = int(st.number_input("n2:"))
+#n2 = int(input("n2:"))
+
+operator = st.selectbox("select operation", ['Add','Subraction','Multiply',"Division",'Power','Floor'])
+
+# a=st.button("click")
+# st.write(a)
+if st.button("Show"):
+
+    if operator == 'Add':
+        st.write(add(n1, n2))
+    elif operator == 'Subraction':
+        st.write(sub(n1, n2))
+    elif operator == 'Multiply':
+        st.write(mull(n1, n2))
+    elif operator == 'Power':
+        st.write(power(n1, n2))
+    elif operator == 'Division':
+        st.write(division(n1, n2))
+    elif operator == 'Floor':
+        st.write(floor(n1,n2))
+    else:
+        st.write("invalid input")
--- helpers.py
+++ helpers.py
@@ -1,19 +1,22 @@
 def greet(name):
     print("hello")
-    print(f"welcomr {name} to the application")
+    print(f"welcome {name} to the application")
     print("Thank you")
-s=input()
-greet(s)
 
 def mull(a,b):
     return a*b
+
 def sub(a,b):
     return a-b
+
 def add(a,b):
     return a+b
+
 def division(a,b):
     return a/b
+
 def power(a,b):
     return a**b
+
 def floor(a,b):
-    return a%b
+    return a//b
